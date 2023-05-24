2d289c7799a313d3b919ab82687ff78427daac26
Danielle Torres <danielletorres@Lumiios-MacBook-Air.local>
2023-05-24 08:50:07 -0700

    E8: Added Basic Alpine Transition to Modal

--- resources/views/components/confirmation-modal.blade.php
+++ resources/views/components/confirmation-modal.blade.php
@@ -1,19 +1,23 @@
 <div {{ $attributes }}
      x-data="{ show: @entangle($attributes->wire('model'))}"
      x-show="show"
+     @keydown.escape.window="show=false"
+     style="display: none"
 >
     <div class="fixed inset-0 bg-gray-900 opacity-90" @click="show=false"></div>
-    <div class="bg-white shadow-md p-4 h-48 max-w-sm m-auto rounded-md fixed inset-0">
+    <div class="bg-white shadow-md h-48 max-w-sm m-auto rounded-md fixed inset-0" x-show.transition="show">
         <div class="flex flex-col h-full justify-between">
-            <header class="font-bold text-lg">
-                {{ $title }}
+            <header class="p-6">
+                <h3 class="font-bold text-lg">
+                    {{ $title }}
+                </h3>
             </header>
 
-            <main >
+            <main class="px-6 mb-4">
                 {{ $body }}
             </main>
 
-            <footer>
+            <footer class="flex justify-end bg-gray-200 px-6 py-4 rounded-b-md">
                 {{ $footer }}
             </footer>
         </div>
